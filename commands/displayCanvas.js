543e86280848c58a7b75683ec26cc5fb58edd73e
vinaycat <vinaymeldrum@gmail.com>
2020-12-31 12:31:37 -0500

    got image to work

--- commands/displayCanvas.js
+++ commands/displayCanvas.js
@@ -5,41 +5,12 @@ module.exports = {
     name: 'canvas',
     description: 'Displays test Canvas',
     async execute(message, args) {
-    let image = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
-       // const collector = new Discord.MessageCollector(message.channel, m => m.content.includes('.'), {
-    //     time: 10000
-    // });
-    // collector.on('collect', async message1 => {
-    const canvas = Canvas.createCanvas(500, 1000);
-    const ctx = canvas.getContext('2d');
-    //console.log('in avatar');
-
-    // Since the image takes time to load, you should await it
-    const background = await Canvas.loadImage(image);
-    // This uses the canvas dimensions to stretch the image onto the entire canvas
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    // Draw a rectangle with the dimensions of the entire canvas
-    ctx.strokeStyle = '#74037b';
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    // Pick up the pen
-    ctx.beginPath();
-    // Start the arc to form a circle
-    //ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-    ctx.arc(310, 310, 95, 0, Math.PI * 2, true);
-    // Put the pen down
-    ctx.closePath();
-    // Clip off the region you drew on
-    ctx.clip();
-
-    // Use helpful Attachment class structure to process the file for you
-    //ctx.drawImage(avatar, 25, 25, 200, 200);
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer());
-    // let embed = new Discord.MessageEmbed()
-    //     //.attachFiles(attachment)
-    //     .setImage('attachment://hangmanImage.png');
-    //     message.channel.send({ embed, files: [attachment] })
-    // console.log(attachment)
-    return attachment;
-    // });
+        let image = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png";
+        const canvas = Canvas.createCanvas(500, 1000);
+        const ctx = canvas.getContext('2d');
+        const background = await Canvas.loadImage(image);
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'test-image.png');
+        return attachment;
     }
 }
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Canvas = require('canvas');
+
 const token = require('./token.js').token
 const fs = require("fs");
 const prefix = "+";
@@ -20,7 +22,16 @@ function embedMessage(output, message) {
         .setDescription(output);
     message.channel.send(embed);
 }
-client.on("message", message => {
+async function displayCanvas(message, args) {
+    let image = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png";
+    const canvas = Canvas.createCanvas(500, 1000);
+    const ctx = canvas.getContext('2d');
+    const background = await Canvas.loadImage(image);
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), './hangmanImage.png');
+    return ['attachment://hangmanImage.png', attachment];
+}
+client.on("message", async message => {
     if (message.content.startsWith(prefix)) {
         const args = message.content.substring(prefix.length).split(" ");
         const command  = args[0].toLowerCase()
@@ -44,12 +55,14 @@ client.on("message", message => {
             client.commands.get('returnPages').execute(list, "ListOfPlayers", message, " "); 
         }
         if(command == "display"){
-            let attachement = client.commands.get('canvas').execute();
+            let attachement = await displayCanvas(message, args);
+            console.log(attachement)
             let embed = new Discord.MessageEmbed();
             embed.setFooter(" ");
+            embed.setImage(attachement[0]);
             message.channel.send({
                 embed,
-                files: [attachement]
+                files: [attachement[1]]
             });
         }
     }
