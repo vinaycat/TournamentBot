dea0899c91a341fe9e8091c05cf97cca5dc07c3b
vinaycat <vinaymeldrum@gmail.com>
2020-12-30 20:17:40 -0500

    added a command handler

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = require('./token.js').token
+const fs = require("fs");
+const prefix = "+";
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
 client.on("ready", async () => {
     client.user.setActivity("Making Tournaments");
 });
 client.on("message", message => {
-    const Prefix = "+";
-    if (message.content.startsWith(Prefix)) {
-        let args = message.content.substring(Prefix.length).split(" ");
-        if(args[0].toLowerCase() == "ping"){
-            message.channel.send("pong!")
+    if (message.content.startsWith(prefix)) {
+        const args = message.content.substring(prefix.length).split(" ");
+        const command  = args[0].toLowerCase()
+        if(command === "ping"){
+            client.commands.get('ping').execute(message, args);
         }
     }
 });
